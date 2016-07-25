a064a4304a63714d6b5656b00251a88f579a2195
Morsdeimos <longden.jonathanedgar@gmail.com>
2016-07-25 13:07:58 -0600

    setting for Passport

---
+++ model/userModal.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var SaleSchema = require('./SaleSchema.js');
+
+var bcrypt = require('bcrypt-nodejs'); //encrypts the password
+
+var UserSchema = new mongoose.Schema({
+
+	userName: {type: String, required: true, unique: true},
+	firstName: {type: String, required: false},
+	lastName: {type: String, required: false},
+	email: {type: String, required: false, unique: true},
+	password: {type: String, required: true},
+	role: {type: String, required: false, default: 'User'},
+	loggedin: {type: Boolean},
+	salePost: [SaleSchema]
+	
+}); 
+
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null); //encrypts password and does so 8 times. Default is 10. More encryptions means more time to process
+};
+
+UserSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password); //compares the given password with the encrypted stored password
+};
+
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ passport/passport.js
@@ -0,0 +1,67 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/userModel.js');
+var passport = require('passport');
+
+module.exports = function(passport) { //call in passport as a parameter
+
+    passport.serializeUser(function(user, done) {//serialize user and passport stores information about the login and session of the user
+        console.log("USER", user);
+        done(null, user.id);//sets info to id
+    });
+    passport.deserializeUser(function(id, done) {//deserializes to return user information after it has been serialized in a language that makes sense to us.
+        console.log("ID", id);
+        User.findById(id, function(err, user) {//searches for info by id
+            done(err, user);
+        });
+    });
+
+    passport.use('local-login', new LocalStrategy({
+    usernameField: 'userName',
+    passwordField: 'password',
+    passReqToCallback: true
+    },
+    function(req, userName, password, done){
+      User.findOne({ userName: userName }, function(err, user){
+        if(err)
+          return done(err);
+        if(!user)
+          return done(null, false, { message: 'We could not find your User ID' });
+        if(!user.validPassword(password))
+          return done(null, false, { message: 'Wrong password. Try again.'});
+        return done(null, user, { message: 'You logged in successfully' });
+      });
+    }));  
+
+    passport.use('local-signup', new LocalStrategy({//use local-signup
+        usernameField : 'userName',//this can be username, email, anything as long as you update all other instances of email on this file.
+        passwordField : 'password',
+        passReqToCallback : true//this makes its so we only need one callback function below
+    },
+    function(req, userName, password, done) {
+        process.nextTick(function() { //waits until all previous code has completed then runs callback function. This is a node function.
+          User.findOne({'userName': userName}, function(err, user) { //find by email mongoose function
+              if (err) return done(err); 
+              if (user) { 
+                if (user.validPassword(password)) {
+                  console.log('Login Success!');
+                    return done(null, user);
+                } else {
+                  console.log('Invalid userName or password');
+                    return done(null, false);
+                }
+              } else { //otherwise, make a new user
+                  var newUser = new User(req.body);
+                  newUser.userName = userName;
+                  newUser.password = newUser.generateHash(password); //hash password
+                  // newUser.username = req.body.username;
+                  // newUser.role = 'guest';
+
+                  newUser.save(function(err) { //save to mongo
+                      if (err) throw err;
+                      return done(null, newUser ,{ message: 'You successfully signed up.' });
+                  });
+              }
+          });
+        });
+    }));
+};
