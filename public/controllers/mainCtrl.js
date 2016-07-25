33c645a6d3924440fa4c96447641a03fa7a6faca
Mike Hohne <mikehohne@gmail.com>
2016-07-25 16:17:04 -0600

    done for the day, got geolocation working

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,4 +1,19 @@
-angular.module("garageApp").controller("mainCtrl", function($scope,mainServ){
+angular.module("garageApp").controller("mainCtrl", function($scope,$window){
 
-
+  $scope.askQuestion = function(){
+    var answer = prompt("Is this Garage Sale at your current location?");
+    answer = answer.toUpperCase();
+    if(answer === "YES"){
+      $window.navigator.geolocation.getCurrentPosition(function(position){
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      $scope.$apply(function(){
+        $scope.lat = lat;
+        $scope.long = long;
+      });
+  })
+    } else {
+      return console.log("SOmething Else");
+    }
+  }
 });
