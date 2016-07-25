070e46a3a9180bd26531a1b048e035331fa8503f
Kyle Hamburg <kylelhamburg@gmail.com>
2016-07-25 15:18:56 -0600

    Added get, post, and update functions in mainServ and mainCtrl

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,4 +1,19 @@
 angular.module("garageApp").controller("mainCtrl", function($scope,mainServ){
+  $scope.sales;
 
+  $scope.getSales = function(){
+    mainServ.getSales()
+    .then(function(res){
+      $scope.sales = res;
+    })
+  }
 
+  $scope.postSale = function(sale){
+  //  angular.copy
+    mainServ.postSale(sale);
+  }
+
+  $scope.updateSale = function(sale){
+    mainServ.updateSale(sale);
+  }
 });
