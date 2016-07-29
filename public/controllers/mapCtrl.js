8eec078ad4a5ca9ec297738dff0ee2e097d6e848
Mike Hohne <mikehohne@gmail.com>
2016-07-29 14:37:56 -0600

    map is working with multiple markers and addresses are showing up

--- public/controllers/mapCtrl.js
+++ public/controllers/mapCtrl.js
@@ -2,70 +2,76 @@ angular.module('garageApp').controller('mapCtrl', function($scope,mainServ){
 
 
 
-// intializing map
+
+  $scope.initMap = function() {
+    $scope.address;
+            var data =[
+              {
+                address: "232 E Main Bozeman"
+              },
+              {
+                address: "414 W Main Bozeman"
+              },
+              {
+                address: "414 Wilson Bozeman"
+              },
+              {
+                address: "211 S 5th Bozeman"
+              },
+              {
+                address: "116  S 10th Bozeman"
+              },
+              {
+                address: "401 S Black Bozeman"
+              }
+            ];
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 12,
+             center: {lat: 45.674788, lng: -111.094465}
+            });
+            var geocoder = new google.maps.Geocoder();
+
+              $scope.geocodeAddress(geocoder, map, data);
+              var result = "";
+              for(var i =1; i<data.length; i++){
+                result += data[i].address;
+              }
+              $scope.address = result;
+              return $scope.address;
+
+            };
 
 
 
 
-//             var map = new google.maps.Map(document.getElementById('map'), {
-//               zoom: 12,
-//               center: {lat: 45.674788, lng: -111.094465}
-//             });
-//             var geocoder = new google.maps.Geocoder();
-//
-//             // document.getElementById('submit').addEventListener('click', function() {
-//             //   geocodeAddress(geocoder, map);
-//             // });
-//
-//
-//
-// // building the geocoding function
-//
-//
-//
-//       $scope.geocodeAddress = function() {
-//         $scope.map;
-//         $scope.address;
-//         var data = {
-//           address: "332 E Main Bozeman"
-//         };
-//         var resultsMap = {};
-//             // var address = document.getElementById('address').value;
-//             // if(address === ""){
-//             //   // return alert("Please insert an address");
-//             // }
-//             // var time = document.getElementById('time').value;
-//             // if(time === ""){
-//             //   // return alert("Please Insert a Time");
-//             // }
-//             // var ampm = document.getElementById('am').value;
-//             // $scope.test;
-//             // $scope.ampm = ampm;
-//             // $scope.address = address;
-//             // $scope.time = "@ " + time;
-//             geocoder.geocode(data, function(results, status) {
-//               if (status === google.maps.GeocoderStatus.OK) {
-//                 $scope.address = data.address;
-//                 console.log($scope.address);
-//                 $scope.map.setCenter(results[0].geometry.location);
-//                 resultsMap.setZoom(17);
-//
-//
-//
-//                 marker = new google.maps.Marker({
-//                   map: resultsMap,
-//                   position: results[0].geometry.location,
-//                   title: 'Garage Sale'
-//
-//                 });
-//
-//
-//
-//               } else {
-//                 alert('Geocode was not successful for the following reason: ' + status);
-//               }
-//             });
-//           }
+
+          $scope.geocodeAddress = function(geocoder, resultsMap, data) {
+
+
+                  for(var i =0; i<data.length; i++){
+                   geocoder.geocode(data[i], function(results, status) {
+                     if (status === google.maps.GeocoderStatus.OK) {
+                       resultsMap.setCenter(results[0].geometry.location);
+                       resultsMap.setZoom(14);
+
+                       marker = new google.maps.Marker({
+                         map: resultsMap,
+                         position: results[0].geometry.location
+                       });
+
+                     } else {
+                       alert('Geocode was not successful for the following reason: ' + status);
+                     }
+
+                   });
+                 }
+               }
+
+
+
+
+
 
 
 });
