d34c6b7b2c4d777632e0c78a43187b9b9932fc55
Mike Hohne <mikehohne@gmail.com>
2016-08-02 13:05:50 -0600

    working with markers

--- public/controllers/mapCtrl.js
+++ public/controllers/mapCtrl.js
@@ -1,36 +1,65 @@
 angular.module('garageApp').controller('mapCtrl', function($scope,mainServ){
- var address;
- $scope.title = "Post";
 
+// initializing variables
+var address;
+var map;
+var geoLocation;
+$scope.title = "Post";
 
-// $scope.initMap = function() {
-//
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 12,
-//     center: {lat: 45.674788, lng: -111.094465}
-//     });
+//projecting the map on the page function
 
-
-    $scope.postSale = function(sale){
-      var newAddress = { address: sale.address};
-      var geocoder = new google.maps.Geocoder();
-      geocoder.geocode(newAddress, function(results,status){
-        if(status === google.maps.GeocoderStatus.OK){
-          console.log(results);
-        } else {
-          console.log("didn't work because of " + status);
-        }
-      })
-      $scope.title = "Saving"
-      window.setTimeout(function(){
-        mainServ.postSale(sale)
-        .then(function(res){
-          console.log(res);
-          sale.address = "";
-          $scope.title = "Saved"
-        })
-      }, 1000)
+  $scope.initMap = function(){
+  var newMap = new google.maps.Map(document.getElementById('map'), {
+  zoom: 12,
+  center: {lat: 45.674788, lng: -111.094465}
+  });
 }
+
+
+
+//new maker that has been pulled from the database.
+$scope.makeMarker = function(newMap, markerPos){
+  marker = new google.maps.Marker({
+   setMap: newMap,
+   position: markerPos,
+   title: "A-Z"
+  });
+};
+
+
+
+
+$scope.postSale = function(sale){
+  var newAddress = { address: sale.address + " Bozeman"};
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode(newAddress, function(results,status){
+    if(status === google.maps.GeocoderStatus.OK){
+      geoLocation = results[0].geometry.location;
+
+      //Calling makeMarker function here with geoLocation as  parameter
+
+      $scope.makeMarker(geoLocation);
+
+    } else {
+      console.log("didn't work because of " + status);
+    }
+  })
+
+  // setting a timeout to avoid 'spatial query limit' error
+
+  $scope.title = "Saving"
+  window.setTimeout(function(){
+    mainServ.postSale(sale)
+    .then(function(res){
+      console.log(res);
+      sale.address = "";
+      $scope.title = "Saved"
+    })
+  }, 1000);
+};
+
+
+
 
 
 
