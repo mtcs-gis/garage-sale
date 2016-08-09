683d04cea4978b56657dd79f347ab855740feacf
Morsdeimos <longden.jonathanedgar@gmail.com>
2016-08-09 11:18:31 -0600

    password reset

--- index.js
+++ index.js
@@ -1,36 +1,133 @@
+var session = require('express-session')
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
+var async = require('async');
+var crypto = require('crypto');
 var express = require('express');
 //var cors = require('cors');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
+var flash = require('express-flash');
 var config = require('./config.js');
+var nodemailer = require('nodemailer'); //password reset
+var sgTransport = require('nodemailer-sendgrid-transport'); //password reset
+var sendgrid  = require('sendgrid'); //password reset
+var User = require('./model/userModel'); // password reset
+var engine = require('consolidate');
 
-var passport = require('passport');
+var options = {
+	auth: {
+		api_key: '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'
+	}
+};
+var mailer = nodemailer.createTransport(sgTransport(options));
+
+
+
 
 var configSession = require('./passport/setsecret.js');
 
 require('./passport/passport.js')(passport);
-
+var userControl = require('./controller/userControl.js');
+var saleControl = require('./controller/saleControl.js');
 
 var app = express();
 
 //app.use(cors);
 
-app.use(session(configSession));
-app.use(passport.initialize());
-app.use(passport.session());
+///////////////////////////////
+//        Middleware         //
+///////////////////////////////
+
+
+// app.set('views', __dirname + '/views');
+// app.engine('html', engine.mustache);
+// app.set('view engine', 'html');
+
+
+//app.set('views', path.join(__dirname, 'views')); //
+//app.set('view engine', 'html'); //
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname+'/public'));
+app.use(cookieParser());
+app.use(session(configSession));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(express.static(__dirname+'/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var userControl = require('./controller/userControl.js');
-var saleControl = require('./controller/saleControl.js')
 
 
 
 app.post('/login', userControl.login);
+app.post('/forgot', userControl.forgot);
+//app.get('reset/:token', userControl.reset);
+
+app.get('/reset/:token', function(req, res) {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('/forgot');
+    }
+    res.render('/#/reset', {
+      user: req.user
+    });
+  });
+});
+
+app.post('/reset/:token', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+
+        user.local.password = req.body.local.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+
+        user.save(function(err) {
+          req.logIn(user, function(err) {
+            done(err, user);
+          });
+        });
+      });
+    },
+    function(user, done) {
+      var mailOptions = {
+        to: user.local.userName,
+        from: 'passwordreset@demo.com',
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+          'This is a confirmation that the password for your account ' + user.local.userName + ' has just been changed.\n'
+      };
+      sendgrid.send(mailOptions, function(err, json) {
+        req.flash('success', 'Success! Your password has been changed.');
+        done(err);
+      });
+    }
+  ], function(err) {
+    res.redirect('/#/reset');
+  });
+});
+
+
+
 app.post('/signup', userControl.signup);
 app.get('/logout', userControl.logout);
+
+
+	
 
 
 
@@ -66,7 +163,9 @@ app.put('/sales:id',saleControl.update);
 app.get('/profile', function(req, res){
 	res.send(req.user);
 });
-
+///////////////////////////////
+//         Routes            //
+///////////////////////////////
 
 
 
